a9155eb4bd0c4ded8134e963e302a2903c28a47d
SANGWOOKHA\Sangwook Ha <swid1@naver.com>
2017-03-30 03:11:09 +0900

    system -> write, deinit시에 pwm duty 초기화 추가

--- ForOdroid/motor.c
+++ ForOdroid/motor.c
@@ -12,17 +12,18 @@ void pwmInit(void){
 		write(freq,temp,3);
 		close(freq);
 	}
-	sleep(1)
+	sleep(1);
 	for(i=0;i<2;i++){
-		sprintf(temp,"echo %d > %s%s%d",PWMMIN,MOTORDIR,DUTY,i);
-		//duty[i] = open(temp,O_WRONLY||O_TRUNC);
+		//sprintf(temp,"echo %d > %s%s%d",PWMMIN,MOTORDIR,DUTY,i);
+		sprintf(temp,"%s%s%d",MOTORDIR,DUTY,i);
+		duty[i] = open(temp,O_WRONLY||O_TRUNC);
 		//printf("%s : %d , ",temp,duty[i]);
-		//sprintf(temp,"%d",PWMMIN);
+		sprintf(temp,"%d",PWMMIN);
 		//printf("duty%d : %s\n",i,temp);
-		//write(duty[i],temp,4);
-		system(temp);
+		write(duty[i],temp,4);
+		//system(temp);
 	}
-	sleep(1)
+	sleep(1);
 	for(i=0;i<2;i++){
 		sprintf(temp,"%s%s%d",MOTORDIR,ENPWM,i);
 		en = open(temp,O_WRONLY||O_TRUNC);
@@ -40,29 +41,29 @@ void pwmSpeed(int flag, int speed){
 	else if(speed < PWMMIN)
 		speed = PWMMIN;
 
-	//sprintf(temp,"%d",speed);
+	sprintf(temp,"%d",speed);
 	//printf("input speed : %s\n",temp);
 	switch(flag){
 		case 0:
-			//write(duty[0],temp,4);
+			write(duty[0],temp,4);
 			//printf("pwm0 %s \n",temp);
-			sprintf(temp,"echo %d > %s%s%d",speed,MOTORDIR,DUTY,0);
-			system(temp);
+			//sprintf(temp,"echo %d > %s%s%d",speed,MOTORDIR,DUTY,0);
+			//system(temp);
 			break;
 		case 1:
-			//write(duty[1],temp,4);
+			write(duty[1],temp,4);
 			//printf("pwm1 %s \n",temp);
-			sprintf(temp,"echo %d > %s%s%d",speed,MOTORDIR,DUTY,1);
+			//sprintf(temp,"echo %d > %s%s%d",speed,MOTORDIR,DUTY,1);
 			system(temp);
 			break;
 		default:
-			//write(duty[0],temp,4);
-			//write(duty[1],temp,4);
+			write(duty[0],temp,4);
+			write(duty[1],temp,4);
 			//printf("pwm0&1 %s \n",temp);
-			sprintf(temp,"echo %d > %s%s%d",speed,MOTORDIR,DUTY,0);
-			system(temp);
-			sprintf(temp,"echo %d > %s%s%d",speed,MOTORDIR,DUTY,1);
-			system(temp);
+			//sprintf(temp,"echo %d > %s%s%d",speed,MOTORDIR,DUTY,0);
+			//system(temp);
+			//sprintf(temp,"echo %d > %s%s%d",speed,MOTORDIR,DUTY,1);
+			//system(temp);
 			break;
 	}
 }
@@ -71,6 +72,12 @@ void pwmDeinit(void){
 	char temp[50];
 	int en = 0,i=0;
 	for(i=0;i<2;i++){
+		sprintf(temp,"%s%s%d",MOTORDIR,DUTY,i);
+		duty[i] = open(temp,O_WRONLY||O_TRUNC);
+		//printf("%s : %d , ",temp,duty[i]);
+		sprintf(temp,"%d",PWMMIN);
+		//printf("duty%d : %s\n",i,temp);
+		write(duty[i],temp,4);
 		sprintf(temp,"%s%s%d",MOTORDIR,ENPWM,i);
 		en = open(temp,O_WRONLY||O_TRUNC);		
 		write(en,"0",1);
